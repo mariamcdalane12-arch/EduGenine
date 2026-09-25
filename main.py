297026ef08f61a6ef434f385acb5d9f4cfe621a0
mariamcdalane12-arch <mariamcdalane12@gmail.com>
2026-09-25 19:22:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
 from google import genai
 from pydantic import BaseModel
 import os
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
